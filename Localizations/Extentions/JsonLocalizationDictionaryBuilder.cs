1bbf2aeea61e3d8f056e84c99f85a9916b974d6e
Sajad khalili <sajad39khalili@gmail.com>
2024-08-04 23:33:04 +0330

    Add files via upload

---
+++ Localizations/Extentions/JsonLocalizationDictionaryBuilder.cs
@@ -0,0 +1,79 @@
+﻿namespace SSF.Extensions.Implementations.Localizations.Extentions;
+
+//public static class JsonLocalizationDictionaryBuilder
+//{
+
+//    /// <summary>
+//    ///     Builds an <see cref="JsonLocalizationDictionaryBuilder" /> from given file.
+//    /// </summary>
+//    /// <param name="filePath">Path of the file</param>
+//    public static ILocalizationDictionary BuildFromFile(string filePath)
+//    {
+//        try
+//        {
+//            var res = File.ReadAllText(filePath);
+//            return BuildFromJsonString(res);
+//        }
+//        catch (Exception ex)
+//        {
+//            throw new Exception("Invalid localization file format: " + filePath, ex);
+//        }
+//    }
+
+//    private static readonly JsonSerializerOptions DeserializeOptions = new JsonSerializerOptions
+//    {
+//        PropertyNameCaseInsensitive = true,
+//        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+//        ReadCommentHandling = JsonCommentHandling.Skip,
+//        AllowTrailingCommas = true
+//    };
+
+//    /// <summary>
+//    ///     Builds an <see cref="JsonLocalizationDictionaryBuilder" /> from given json string.
+//    /// </summary>
+//    /// <param name="jsonString">Json string</param>
+//    public static ILocalizationDictionary BuildFromJsonString(string jsonString)
+//    {
+//        JsonLocalizationFile jsonFile;
+//        try
+//        {
+//            jsonFile = JsonSerializer.Deserialize<JsonLocalizationFile>(jsonString, DeserializeOptions);
+//        }
+//        catch (JsonException ex)
+//        {
+//            throw new Exception("Can not parse json string. " + ex.Message);
+//        }
+
+//        var cultureCode = jsonFile.Culture;
+//        if (string.IsNullOrEmpty(cultureCode))
+//        {
+//            throw new Exception("Culture is empty in language json file.");
+//        }
+
+//        var dictionary = new Dictionary<string, LocalizedString>();
+//        var dublicateNames = new List<string>();
+//        foreach (var item in jsonFile.Texts)
+//        {
+//            if (string.IsNullOrEmpty(item.Key))
+//            {
+//                throw new Exception("The key is empty in given json string.");
+//            }
+
+//            if (dictionary.GetOrDefault(item.Key) != null)
+//            {
+//                dublicateNames.Add(item.Key);
+//            }
+
+//            dictionary[item.Key] = new LocalizedString(item.Key, item.Value.NormalizeLineEndings());
+//        }
+
+//        if (dublicateNames.Count > 0)
+//        {
+//            throw new Exception(
+//                "A dictionary can not contain same key twice. There are some duplicated names: " +
+//                dublicateNames.JoinAsString(", "));
+//        }
+
+//        return new StaticLocalizationDictionary(cultureCode, dictionary);
+//    }
+//}
---
+++ Localizations/JsonStringLocalizer.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace SSF.Extensions.Implementations.Localizations;
+public class JsonStringLocalizer : IStringLocalizer
+{
+
+    //public JsonStringLocalizer(ILogger<JsonStringLocalizer> logger)
+    //{
+    //    this._logger = logger;
+    //}
+    public JsonStringLocalizer()
+    {
+
+    }
+    public JsonStringLocalizer(JsonResourceManager jsonResourceManager/*, ILogger<JsonStringLocalizer> logger*/)
+    {
+        // this._logger = logger;
+        this.jsonResourceManager=jsonResourceManager;
+    }
+
+    private readonly JsonResourceManager jsonResourceManager;
+    private readonly ILogger<JsonStringLocalizer> _logger;
+
+    public LocalizedString this[string name]
+    {
+        get
+        {
+            string value = GetString(name);
+            return new LocalizedString(name, value??name, value==null);
+        }
+    }
+
+    public LocalizedString this[string name, params object[] arguments]
+    {
+        get
+        {
+            var actualValue = this[name];
+            return !actualValue.ResourceNotFound
+                ? new LocalizedString(name, string.Format(actualValue.Value, arguments), false)
+                : actualValue;
+        }
+    }
+
+    //public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+    //{
+
+    //    var cultureInfo = CultureInfo.CurrentUICulture;
+    //    var resourceName = $"{_resourceType.FullName}/{cultureInfo}.json";
+
+    //    Assembly assembly = _resourceType.Assembly;
+    //    string filePath = $"Resources/{_resourceType.Name}/{cultureInfo.Name}.json";
+
+    //    using (var str = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+    //    using (var sReader = new StreamReader(str))
+    //    using (var reader = new JsonTextReader(sReader))
+    //    {
+    //        while (reader.Read())
+    //        {
+    //            if (reader.TokenType != JsonToken.PropertyName)
+    //                continue;
+    //            string key = (string)reader.Value;
+    //            reader.Read();
+    //            string value = _serializer.Deserialize<string>(reader);
+    //            yield return new LocalizedString(key, value, false);
+    //        }
+    //    }
+    //}
+
+    private string GetString(string key)
+    {
+
+        return jsonResourceManager.GetString(key);
+
+    }
+
+    private void LoadData()
+    {
+
+    }
+
+    public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+    {
+        throw new NotImplementedException();
+    }
+}
